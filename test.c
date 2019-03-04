e7974a3f1ed1a521b77a45d0ed07e7576e021873
jagrutik9 <48032608+jagrutik9@users.noreply.github.com>
2019-03-04 16:35:42 +0100

    Add files via upload

--- test.c
+++ test.c
@@ -1,8 +1,43 @@
-//TEST
 #include<stdio.h>
+#include<stdlib.h>
 
-int main()
+int16_t _16bit_unsigned(int lsb, int msb);
+float calculate_mtof(float mtof);
+float calculate_float(unsigned char byte_array[4]);
+
+
+int main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+	int lsb = 12, msb = 2;
+    int serial = 79;
+	int16_t data1;
+	float data2, x;
+    unsigned char arr[] = {0x10, 0x20, 0x30, 0x40};
+    x = calculate_float(arr);
+	data1 = _16bit_unsigned(lsb, msb);
+	data2 = calculate_mtof(serial);
+	printf("Converted 16_bit unsigned Value: %d\n", data1);
+	printf("MTOF Value: %f\n", data2);
+	printf("Array to float Value: %f\n", x);
+	return 0;
+}
+
+int16_t _16bit_unsigned(int lsb, int msb)
+{
+	int16_t value;
+	value = (msb << 8) + lsb;
+	return value;
+}
+
+float calculate_mtof(float mtof)
+{
+	float value = mtof / (float)3.0;
+	return value;
+}
+
+float calculate_float(unsigned char byte_array[4])
+{
+    float out;
+	out = (byte_array[0] << 24) | (byte_array[1] << 16) | (byte_array[2] << 8) | (byte_array[3]);
+	return out;  
 }
